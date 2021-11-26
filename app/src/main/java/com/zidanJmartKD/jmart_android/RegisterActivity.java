a70a8147c3597b40b8cffd506d4b590a279ba12d
Zidan Ramadhan <zidan.ramadhan@ui.ac.id>
2021-11-26 18:28:55 +0700

    TP modul9 Zidan Ramadhan

---
+++ app/src/main/java/com/zidanJmartKD/jmart_android/RegisterActivity.java
@@ -0,0 +1,16 @@
+package com.zidanJmartKD.jmart_android;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.zidanJmartKD.R;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
